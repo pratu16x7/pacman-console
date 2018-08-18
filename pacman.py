9ef286cbdf766cadfbfe2fc9a66b44dc6e9fa6ca
Prateeksha Singh <pratu16x7@gmail.com>
2018-08-18 21:01:00 +0530

    [movement] add pacman character progression

--- pacman.py
+++ pacman.py
@@ -2,6 +2,26 @@ import random
 import curses
 import time
 
+class CharProgression:
+    def __init__(self, name, progression_cycle):
+        self.name = name
+        self.cycle = progression_cycle
+        self.current_index = 0
+
+    def get_char(self):
+        return self.cycle[self.current_index]
+
+    def update(self, reverse=False):
+        if not reverse:
+            self.current_index += 1
+            if self.current_index >= len(self.cycle):
+                self.current_index = 0
+        else:
+            self.current_index -= 1
+            if self.current_index <= 0:
+                self.current_index = len(self.cycle) - 1
+
+# SETUP
 s = curses.initscr()
 curses.curs_set(0)
 
@@ -28,11 +48,22 @@ box1.box()
 #         curses.ACS_PI
 #     )
 
+map1 = []
+
 bounds = [0, sh/2, 0, sw/2]
 
-new_pos = [sh/2, sw/2]
 
-key = curses.KEY_UP
+# CHAR PROGRESSIONS
+right_progression = CharProgression('right', ['{', '(', '<', '-', '-', '<', '(', '{'])
+left_progression = CharProgression('left', ['}', ')', '>', '-', '-', '>', ')', '}'])
+up_progression = CharProgression('up', ['v', 'V', '|', '|', 'V'])
+down_progression = CharProgression('down', ['^'])
+
+# INIT
+new_pos = [sh/2, sw/2]
+current_progression = right_progression
+
+key = curses.KEY_RIGHT
 
 while True:
 
@@ -43,13 +74,22 @@ while True:
     old_pos = new_pos[:]
 
     if key == curses.KEY_UP and not new_pos[0] <= bounds[0] + offset_y:
+        current_progression = up_progression
         new_pos[0] -= 1
+
     if key == curses.KEY_DOWN and not new_pos[0] >= bounds[1] + offset_y:
+        current_progression = down_progression
         new_pos[0] += 1
+
     if key == curses.KEY_LEFT and not new_pos[1] <= bounds[2] + offset_x:
+        current_progression = left_progression
         new_pos[1] -= 2
+
     if key == curses.KEY_RIGHT and not new_pos[1] >= bounds[3] + offset_x:
+        current_progression = right_progression
         new_pos[1] += 2
+
+    current_progression.update()
 
     s.addch(
         old_pos[0],
@@ -60,7 +100,7 @@ while True:
     s.addch(
         new_pos[0],
         new_pos[1],
-        curses.ACS_LARROW
+        current_progression.get_char()
     )
 
     s.getch()
