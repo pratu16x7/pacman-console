43cad05e8ae37d5c107b925f7cc80654c057cc7c
Prateeksha Singh <pratu16x7@gmail.com>
2018-08-22 13:49:46 +0530

    render map from a map file

--- objects/game_box.py
+++ objects/game_box.py
@@ -1,12 +1,53 @@
-
-
 class GameBox:
     def __init__(self, screen):
-        # self.subwin = screen.subwin(
-        #     height,
-        #     width,
-        #     y,
-        #     x
-        # )
-        pass
+        self.screen = screen
+        self.init_map_matrix()
+        self.init_map_box()
+        self.draw_map()
+
+
+    def init_map_matrix(self):
+        self.map_matrix = []
+        # with open('../maps/map.txt') as map_file:
+        with open('maps/map.txt') as map_file:
+            for line in map_file:
+                # trim the new_line char
+                self.map_matrix.append(list(line)[:-1])
+
+        # # TODO:
+        # # Better checks to validate map files, to parse them seamlessly
+
+
+    def init_map_box(self):
+        screen_h, screen_w = self.screen.getmaxyx()
+
+        m = self.map_matrix
+        map_h, map_w = len(m), len(m[0])
+
+        offset_y = int((screen_h - map_h)/2)
+        offset_x = int((screen_w - map_w)/2)
+
+        self.map_box = self.screen.subwin(
+            map_h,
+            map_w,
+            offset_y,
+            offset_x
+        )
+
+        self.map_box.keypad(1)
+        self.map_box.timeout(100)
+
+
+    def draw_map(self):
+        line_index = 0
+        for line in self.map_matrix:
+            char_index = 0
+            for char in line:
+                self.map_box.addch(
+                    line_index,
+                    char_index,
+                    char
+                )
+                char_index += 1
+            line_index += 1
 
--- pacman.py
+++ pacman.py
@@ -1,83 +1,66 @@
-import random
 import curses
-import time
+# import random
+# import time
 from objects.character import Pacman
 from objects.game_box import GameBox
 
 
 class PacmanGame:
     def __init__(self):
-        self.init_curses()
-        self.init_screen()
+        self.init_curses_and_screen()
         self.init_map()
-        self.init_characters()
+        # self.init_characters()
         self.start()
 
 
-    def init_curses(self):
+    def init_curses_and_screen(self):
         self.screen_obj = curses.initscr()
         curses.curs_set(0)
         curses.noecho()
+        self.screen_obj.border(0)
 
 
-    def init_screen(self):
-        s = self.screen_obj
-        s.border(0)
-
-        h, w = s.getmaxyx()
-
-        self.screen = {
-            'height': h,
-            'width': w,
-            'offset_y': int(h/4),
-            'offset_x': int(w/4),
-            'bounds': [2, h/2 - 3, 2, w/2 + 1]
-        }
-
+    # def init_screen(self):
+    #     self.screen = {
+    #         'height': h,
+    #         'width': w,
+    #         'offset_y': int(h/4),
+    #         'offset_x': int(w/4),
+    #         'bounds': [2, h/2 - 3, 2, w/2 + 1]
+    #     }
 
     def init_map(self):
-        s = self.screen
-        # self.game_box = GameBox(self.screen_obj)
-
-        self.game_box = self.screen_obj.subwin(
-            int(s.get('height')/2 + 3),
-            int(s.get('width')/2 + 6),
-            int(s.get('offset_y') - 1),
-            int(s.get('offset_x') - 2)
-        )
-        self.game_box.box()
-        self.game_box.keypad(1)
-        self.game_box.timeout(100)
+        self.game_box = GameBox(self.screen_obj)
 
 
-    def init_characters(self):
-        # TODO: remove
-        s = self.screen
+    # def init_characters(self):
+    #     # TODO: remove
+    #     s = self.screen
 
-        self.pacman = Pacman(
-            self.game_box,
-            [s.get('height')/2, s.get('width')/2],
-            s
-        )
+    #     self.pacman = Pacman(
+    #         self.game_box,
+    #         [s.get('height')/2, s.get('width')/2],
+    #         s
+    #     )
 
 
     def start(self):
         key = curses.KEY_RIGHT
         while True:
-            next_key = self.game_box.getch()
-            key = key if next_key == -1 else next_key
+            next_key = self.game_box.map_box.getch()
+            # key = key if next_key == -1 else next_key
 
-            if key == curses.KEY_UP:
-                self.pacman.move('UP')
+            # if key == curses.KEY_UP:
+            #     self.pacman.move('UP')
 
-            if key == curses.KEY_DOWN:
-                self.pacman.move('DOWN')
+            # if key == curses.KEY_DOWN:
+            #     self.pacman.move('DOWN')
 
-            if key == curses.KEY_LEFT:
-                self.pacman.move('LEFT')
+            # if key == curses.KEY_LEFT:
+            #     self.pacman.move('LEFT')
 
-            if key == curses.KEY_RIGHT:
-                self.pacman.move('RIGHT')
+            # if key == curses.KEY_RIGHT:
+            #     self.pacman.move('RIGHT')
 
             self.screen_obj.getch()
 
